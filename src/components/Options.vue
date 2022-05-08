08b919b6435d56af50cbfb47c89731edf373dc36
Tomasz <tomaszjanik17@gmail.com>
2022-05-09 00:03:01 +0200

    refator(components): small reafactor

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,121 +1,119 @@
 <template>
   <transition
-      enter-active-class="transition-all transform"
-      leave-active-class="transition-all transform"
-      enter-from-class="translate-x-40"
-      enter-to-class="translate-x-0"
-      leave-from-class="translate-x-0"
-      leave-to-class="translate-x-40">
-    <div v-if="menuOpen"
-         class="fixed right-0 h-full w-40 p-2 flex flex-col bg-white bg-opacity-50 backdrop-blur text-gray-700 z-10">
+    enter-active-class="transition-all transform"
+    leave-active-class="transition-all transform"
+    enter-from-class="translate-x-40"
+    enter-to-class="translate-x-0"
+    leave-from-class="translate-x-0"
+    leave-to-class="translate-x-40"
+  >
+    <div
+      v-if="menuOpen"
+      class="fixed right-0 h-full w-40 p-2 flex flex-col bg-white bg-opacity-50 backdrop-blur text-gray-700 z-10"
+    >
       <div class="font-bold mb-1 text-center">Zoom</div>
       <div class="flex flex-row justify-around items-center">
-        <button @click="$emit('update:zoom', Math.max(zoom - 10,0))"
-                class="p-1 rounded-full hover:bg-gray-500 hover:bg-opacity-25 focus:outline-none focus:ring">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="1em" height="1em">
-            <path fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"/>
-            <path fill-rule="evenodd" d="M5 8a1 1 0 011-1h4a1 1 0 110 2H6a1 1 0 01-1-1z" clip-rule="evenodd"/>
-          </svg>
+        <button
+          class="p-1 rounded-full hover:bg-gray-500 hover:bg-opacity-25 focus:outline-none focus:ring"
+          @click="$emit('update:zoom', Math.max(zoom - 10, 0))"
+        >
+          <ZoomOutIcon />
         </button>
         <div class="text-sm">{{ zoom }}%</div>
-        <button @click="$emit('update:zoom', zoom + 10)"
-                class="p-1 rounded-full hover:bg-gray-500 hover:bg-opacity-25 focus:outline-none focus:ring">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="1em" height="1em">
-            <path d="M5 8a1 1 0 011-1h1V6a1 1 0 012 0v1h1a1 1 0 110 2H9v1a1 1 0 11-2 0V9H6a1 1 0 01-1-1z"/>
-            <path fill-rule="evenodd"
-                  d="M2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8zm6-4a4 4 0 100 8 4 4 0 000-8z"
-                  clip-rule="evenodd"/>
-          </svg>
+        <button
+          class="p-1 rounded-full hover:bg-gray-500 hover:bg-opacity-25 focus:outline-none focus:ring"
+          @click="$emit('update:zoom', zoom + 10)"
+        >
+          <ZoomInIcon />
         </button>
       </div>
       <div class="font-bold mb-1 text-center">Choose size</div>
       <div class="flex flex-col text-sm space-y-1 mb-2">
-        <button v-for="(option, index) in options" :key="option.width"
-                class="px-2 py-1 focus:outline-none rounded text-xs transition-colors duration-75 focus:ring"
-                @click="$emit('update:chosen-option', index)"
-                :class="chosenOption === index ? 'bg-gray-500 bg-opacity-40' : 'hover:bg-gray-500 hover:bg-opacity-25'">
+        <button
+          v-for="(option, index) in options"
+          :key="option.width"
+          class="px-2 py-1 focus:outline-none rounded text-xs transition-colors duration-75 focus:ring"
+          :class="
+            chosenOption === index
+              ? 'bg-gray-500 bg-opacity-40'
+              : 'hover:bg-gray-500 hover:bg-opacity-25'
+          "
+          @click="$emit('update:chosen-option', index)"
+        >
           {{ option.width }}x{{ option.height }} ({{ option.bombs }})
         </button>
       </div>
-      <button @click="$emit('start-game')"
-              class="px-2 py-1 border-2 border-gray-700 shadow-lg rounded font-bold mb-1 focus:outline-none focus:ring hover:bg-black hover:bg-opacity-10">
+      <button
+        class="px-2 py-1 border-2 border-gray-700 shadow-lg rounded font-bold mb-1 focus:outline-none focus:ring hover:bg-black hover:bg-opacity-10"
+        @click="$emit('start-game')"
+      >
         New game
       </button>
-      <button :class="gameInProgress && 'hover:bg-black hover:bg-opacity-10'"
-              :disabled="!gameInProgress"
-              class="px-2 py-1 border-2 border-gray-700 shadow-lg rounded font-bold mb-1 focus:outline-none focus:ring  disabled:opacity-50"
-              @click="$emit('save-game')">
+      <button
+        :class="gameInProgress && 'hover:bg-black hover:bg-opacity-10'"
+        :disabled="!gameInProgress"
+        class="px-2 py-1 border-2 border-gray-700 shadow-lg rounded font-bold mb-1 focus:outline-none focus:ring disabled:opacity-50"
+        @click="$emit('save-game')"
+      >
         Save
       </button>
       <div class="font-bold mb-1 text-center">Saved Games</div>
       <div class="flex flex-col text-sm space-y-1 mb-2 overflow-auto">
-        <button v-for="(game) in formattedSavedGames" :key="game.id" @click="$emit('load-game',game.id)"
-                class="px-2 py-1 focus:outline-none rounded text-xs transition-colors duration-75 focus:ring hover:bg-gray-500 hover:bg-opacity-25">
+        <button
+          v-for="game in formattedSavedGames"
+          :key="game.id"
+          class="px-2 py-1 focus:outline-none rounded text-xs transition-colors duration-75 focus:ring hover:bg-gray-500 hover:bg-opacity-25"
+          @click="$emit('load-game', game.id)"
+        >
           {{ game.title }}
         </button>
       </div>
     </div>
   </transition>
   <button
-      :class="menuOpen && '-rotate-90 -translate-x-40'"
-      class="fixed top-4 right-4 bg-white bg-opacity-50 backdrop-blur  hover:bg-opacity-70 transition-transform text-gray-700 transform  focus:outline-none focus:ring w-8 h-8 rounded-full flex justify-center items-center text-xl z-10"
-      @click="menuOpen = !menuOpen">
-    <svg v-if="menuOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-         width="1em" height="1em">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-    </svg>
-    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-         stroke="currentColor" width="1em"
-         height="1em">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-    </svg>
+    :class="menuOpen && '-rotate-90 -translate-x-40'"
+    class="fixed top-4 right-4 bg-white bg-opacity-50 backdrop-blur hover:bg-opacity-70 transition-transform text-gray-700 transform focus:outline-none focus:ring w-8 h-8 rounded-full flex justify-center items-center text-xl z-10"
+    @click="menuOpen = !menuOpen"
+  >
+    <CloseIcon v-if="menuOpen" />
+    <CogIcon v-else />
   </button>
 </template>
 
-<style>
+<script setup>
+import { computed, ref, toRefs } from "vue";
+import CogIcon from "@/components/icons/CogIcon.vue";
+import CloseIcon from "@/components/icons/CloseIcon.vue";
+import ZoomOutIcon from "@/components/icons/ZoomOutIcon.vue";
+import ZoomInIcon from "@/components/icons/ZoomInIcon.vue";
 
-</style>
+defineEmits([
+  "start-game",
+  "save-game",
+  "load-game",
+  "update:zoom",
+  "update:chosen-option",
+]);
 
-<script>
+const props = defineProps({
+  gameInProgress: Boolean,
+  zoom: { type: Number, default: 100 },
+  options: { type: Array, default: () => [] },
+  chosenOption: { type: Number, default: 0 },
+  savedGames: { type: Array, default: () => [] },
+});
 
-export default {
-  props: {
-    gameInProgress: Boolean,
-    zoom: Number,
-    options: Array,
-    chosenOption: Number,
-    savedGames: Array
-  },
-  emits: [
-    'start-game',
-    'save-game',
-    'load-game',
-    'update:zoom',
-    'update:chosen-option'
-  ],
-  computed: {
-    formattedSavedGames() {
-      return this.savedGames.map(gameId => {
-        const ms = parseInt(gameId.slice(5));
-        console.log(gameId);
-        console.log(ms);
-        const date = new Date(ms);
-        console.log(date);
-        return {
-          id: gameId,
-          title: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-        };
-      })
-    }
-  },
-  data: () => {
+const { savedGames } = toRefs(props);
+
+const formattedSavedGames = computed(() => {
+  return savedGames.value.map((gameId) => {
+    const gameData = JSON.parse(localStorage.getItem(gameId));
     return {
-      menuOpen: true,
-    }
-  },
-}
+      id: gameId,
+      title: new Date(gameData.lastSavedOn).toLocaleString(),
+    };
+  });
+});
+
+const menuOpen = ref(true);
 </script>
--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,38 +1,45 @@
 <template>
   <div
-      class="fixed top-4 left-1/2 transform -translate-x-1/2 px-4 py-2 backdrop-blur rounded bg-white bg-opacity-75 flex flex-row justify-center items-center font-bold uppercase text-gray-700"
-      :class="{'bg-red-500': result === 'lose', 'bg-green-500': result === 'win'}">
-    <template v-if="result.length">{{ result === 'win' ? 'You won!' : 'You lost!' }}</template>
+    class="fixed top-4 left-1/2 transform -translate-x-1/2 px-4 py-2 backdrop-blur rounded bg-white bg-opacity-75 flex flex-row justify-center items-center font-bold uppercase text-gray-700"
+    :class="{
+      'bg-red-500': result === 'lose',
+      'bg-green-500': result === 'win',
+    }"
+  >
+    <template v-if="result.length">{{
+      result === "win" ? "You won!" : "You lost!"
+    }}</template>
     <template v-else>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="1em" height="1em">
-        <path fill-rule="evenodd"
-              d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-              clip-rule="evenodd"/>
-      </svg>
+      <FlagIcon />
       <div class="ml-2">{{ bombs }}</div>
     </template>
-    <div class="ml-4 mr-4 w-0.5 float-none bg-gray-700 rounded-full">{{"&nbsp;"}}</div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="1em" height="1em">
-      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-    </svg>
+    <div class="ml-4 mr-4 w-0.5 float-none bg-gray-700 rounded-full">
+      {{ "&nbsp;" }}
+    </div>
+    <ClockIcon />
     <span class="ml-2">{{ formattedTime }}</span>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    bombs: Number,
-    result: String,
-    time: {
-      type: Number,
-      default: 0
-    }
+<script setup>
+import FlagIcon from "@/components/icons/FlagIcon.vue";
+import ClockIcon from "@/components/icons/ClockIcon.vue";
+import { computed, toRefs } from "@vue/reactivity";
+
+const props = defineProps({
+  bombs: { type: Number, default: 0 },
+  result: { type: String, default: "" },
+  time: {
+    type: Number,
+    default: 0,
   },
-  computed: {
-    formattedTime() {
-      return `${Math.floor(this.time / 60)}:` + `${Math.floor(this.time % 60)}`.padStart(2, '0');
-    }
-  }
-}
+});
+
+const { time } = toRefs(props);
+
+const formattedTime = computed(
+  () =>
+    `${Math.floor(time.value / 60)}:` +
+    `${Math.floor(time.value % 60)}`.padStart(2, "0")
+);
 </script>
